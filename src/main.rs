a92f68aece63405d99f42116c493caf2f4738d93
NameTakenBonk <83465599+NameTakenBonk@users.noreply.github.com>
2023-02-06 20:40:30 +0000

    Instance

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@ clippy::too_many_arguments,
 clippy::unnecessary_wraps
 )]
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
+use vulkanalia::loader::{LibloadingLoader, LIBRARY};
+use vulkanalia::prelude::v1_0::*;
+use vulkanalia::window as vk_window;
 use winit::dpi::LogicalSize;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
@@ -14,15 +17,13 @@ use winit::window::{Window, WindowBuilder};
 fn main() -> Result<()> {
     pretty_env_logger::init();
 
-    // Window
 
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
-        .with_title("Vulkan Tutorial (Rust)")
+        .with_title(":DDDDDDDDDDDDDDDDDDDDDDDDDDDDDD")
         .with_inner_size(LogicalSize::new(1024, 768))
         .build(&event_loop)?;
 
-    // App
 
     let mut app = unsafe { App::create(&window)? };
     let mut destroying = false;
@@ -43,25 +44,60 @@ fn main() -> Result<()> {
     });
 }
 
-/// Our Vulkan app.
 #[derive(Clone, Debug)]
-struct App {}
+struct App {
+    entry: Entry,
+    instance: Instance,
+}
 
 impl App {
-    /// Creates our Vulkan app.
     unsafe fn create(window: &Window) -> Result<Self> {
-        Ok(Self {})
+        let loader = LibloadingLoader::new(LIBRARY)?;
+        let entry = Entry::new(loader).map_err(|b| anyhow!("{}", b))?;
+        let instance = create_instance(window, &entry)?;
+        Ok(Self { entry, instance })
     }
 
-    /// Renders a frame for our Vulkan app.
     unsafe fn render(&mut self, window: &Window) -> Result<()> {
         Ok(())
     }
 
-    /// Destroys our Vulkan app.
-    unsafe fn destroy(&mut self) {}
+    unsafe fn destroy(&mut self) {
+        self.instance.destroy_instance(None);
+    }
 }
 
-/// The Vulkan handles and associated properties used by our Vulkan app.
 #[derive(Clone, Debug, Default)]
 struct AppData {}
+
+unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance> {
+    let application_info = vk::ApplicationInfo::builder()
+        .application_name(b"ABE Showcase app thingy\0")
+        .application_version(vk::make_version(1, 0, 0))
+        .engine_name(b"Alternative's Bad Engine\0")
+        .engine_version(vk::make_version(1, 0, 0))
+        .api_version(vk::make_version(1, 0, 0));
+
+    let mut extensions = vk_window::get_required_instance_extensions(window)
+        .iter()
+        .map(|e| e.as_ptr())
+        .collect::<Vec<_>>();
+
+    let flags = if entry
+        .enumerate_instance_extension_properties(None)?
+        .iter()
+        .any(|e| e.extension_name == vk::KHR_PORTABILITY_ENUMERATION_EXTENSION.name)
+    {
+        extensions.push(vk::KHR_PORTABILITY_ENUMERATION_EXTENSION.name.as_ptr());
+        vk::InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR
+    } else {
+        vk::InstanceCreateFlags::empty()
+    };
+
+    let info = vk::InstanceCreateInfo::builder()
+        .application_info(&application_info)
+        .enabled_extension_names(&extensions)
+        .flags(flags);
+
+    Ok(entry.create_instance(&info, None)?)
+}
