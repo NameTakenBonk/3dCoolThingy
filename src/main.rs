bbfd49df351c6963e31aeba60ebadf67fd3f0805
NameTakenBonk <83465599+NameTakenBonk@users.noreply.github.com>
2023-02-07 21:08:51 +0000

    Logical device and queues

--- src/main.rs
+++ src/main.rs
@@ -53,6 +53,7 @@ struct App {
     entry: Entry,
     instance: Instance,
     data: AppData,
+    device, Device,
 }
 
 impl App {
@@ -61,8 +62,9 @@ impl App {
         let entry = Entry::new(loader).map_err(|b| anyhow!("{}", b))?;
         let mut data = AppData::default();
         let instance = create_instance(window, &entry, &mut data)?;
+        let device = create_logice_device(&instance, &mut data)?;
         pick_physical_device(&instance, &mut data)?;
-        Ok(Self { entry, instance, data })
+        Ok(Self { entry, instance, data, device })
     }
 
     unsafe fn render(&mut self, window: &Window) -> Result<()> {
@@ -71,6 +73,8 @@ impl App {
 
     #[rustfmt::skip]
     unsafe fn destroy(&mut self) {
+        self.device.destroy_device(None);
+
         if VALIDATION_ENABLED {
             self.instance.destroy_debug_utils_messenger_ext(self.data.messenger, None);
         }
@@ -83,6 +87,7 @@ impl App {
 struct AppData {
     messenger: vk::DebugUtilsMessengerEXT,
     physical_device: vk::PhysicalDevice,
+    graphics_queue: vk::Queue,
 }
 
 unsafe fn create_instance(window: &Window, entry: &Entry, data: &mut AppData) -> Result<Instance> {
@@ -203,6 +208,38 @@ unsafe fn check_physical_device(
 ) -> Result<()> {
     QueueFamilyIndices::get(instance, data, physical_device)?;
     Ok(())
+}
+
+unsafe fn create_logical_device(
+    instance: &Instance,
+    data: &mut AppData,
+) -> Result<Device> {
+    let indices = QueueFamilyIndices::get(instance, data, data.physical_device)?;
+
+    let queue_priorities = &[1.0];
+    let queue_info = vk::DeviceQueueCreateInfo::builder()
+        .queue_family_index(indices.graphics)
+        .queue_priorities(queue_priorities);
+
+    let layers = if VALIDATION_ENABLED {
+        vec![VALIDATION_LAYER.as_ptr()]
+    } else {
+        vec![]
+    };
+
+    let features = vk::PhysicalDeviceFeatures::builder();
+
+    let queue_infos = &[queue_info];
+    let info = vk::DeviceCreateInfo::builder()
+        .queue_create_infos(queue_infos)
+        .enabled_layer_names(&layers)
+        .enabled_features(&features);
+
+    let device = instance.create_device(data.physical_device, &info, None)?;
+
+    data.graphics_queue = device.get_device_queue(indices.graphics, 0);
+
+    Ok(device)
 }
 
 #[derive(Copy, Clone, Debug)]
